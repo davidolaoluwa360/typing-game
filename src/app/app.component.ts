2dbc7a24110af1603271b78ed6f0ae912fdb6a0f
davidolaoluwa360 <d.atebisun@alustudent.com>
2021-05-10 14:44:52 +0200

    first init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
+import { lorem } from 'faker';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typing-game';
+  title = 'Typing Challenge';
+  randomText: string = lorem.sentence();
+  enteredText: string = '';
+
+  onInput(value: any) {
+    value = value.target.value;
+    this.enteredText = value;
+  }
+
+  compare(randomLetter: string, enteredLetter: string):string {
+    if (!enteredLetter) {
+      return 'pending';
+    }
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect';
+  }
 }
